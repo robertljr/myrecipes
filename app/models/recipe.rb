e697e473b69f5cfacad77ca56b427c8201af2adc
robertljr <robert.leejr@gmail.com>
2015-05-22 01:59:09 +0000

    Updated files

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ActiveRecord::Base
-  
+  validates :name, presence: true, length: {minumum: 5, maximum: 100 }
+  validates :summary, presence: true, length: {minumum: 10, maximum: 150 }
+  validates :description, presence: true, length: {minumum: 20, maximum: 500 }
 end
